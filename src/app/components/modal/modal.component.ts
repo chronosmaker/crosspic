cadf681e7e414443adcc8772449e981afca75250
chronosmaker <chronosmaker@126.com>
2017-08-30 18:25:10 +0800

    modal功能调试

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,18 +1,47 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.less']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
 
-  @Input()
   show = false;
+  active = false;
+  observable: any;
+  unsubscribe: any;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.unsubscribe();
+  }
+
+  open() {
+    this.show = true;
+    this.change();
+  }
+
+  close() {
+    this.show = false;
+    this.change();
+  }
+
+  change() {
+    if (this.unsubscribe) {
+      this.unsubscribe.unsubscribe();
+    }
+    this.active = true;
+    this.observable = Observable.timer(1000);
+    this.unsubscribe = this.observable.subscribe(() => {
+      this.active = false;
+    });
   }
 
 }
