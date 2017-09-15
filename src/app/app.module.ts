9a087d4062715bf8ed7480af12a891950d75db77
chronosmaker <chronosmaker@126.com>
2017-09-15 18:17:55 +0800

    module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
 import {StoreModule} from '@ngrx/store';
 import {EffectsModule} from '@ngrx/effects';
 import {StoreRouterConnectingModule, RouterStateSerializer} from '@ngrx/router-store';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
-import {reducers, metaReducers} from './reducers';
-import {CustomRouterStateSerializer} from './shared/utils';
 import {AppRoutingModule} from './app-routing.module';
 import {environment} from '../environments/environment';
+import {reducers, metaReducers} from './reducers';
+import {CustomRouterStateSerializer} from './shared/utils';
 
 import {SystemService} from './shared/system.service';
 import {AppComponent} from './app.component';
