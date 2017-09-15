9ff6057692a509b5e6e368a9400ea212d7cb694a
chronosmaker <chronosmaker@126.com>
2017-09-15 18:10:24 +0800

    module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreRouterConnectingModule, RouterStateSerializer} from '@ngrx/router-store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+
+import {reducers, metaReducers} from './reducers';
+import {CustomRouterStateSerializer} from './shared/utils';
 import {AppRoutingModule} from './app-routing.module';
+import {environment} from '../environments/environment';
+
+import {SystemService} from './shared/system.service';
 import {AppComponent} from './app.component';
-import {HttpClientModule} from "@angular/common/http";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
-import {HashLocationStrategy, LocationStrategy} from "@angular/common";
 import {DrawerComponent} from './components/drawer/drawer.component';
 import {HomeComponent} from './pages/home/home.component';
-import {SystemService} from "./shared/system.service";
 import {CrosspicComponent} from './pages/crosspic/crosspic.component';
 import {MissionComponent} from './pages/crosspic/mission/mission.component';
 import {PlayComponent} from './pages/crosspic/play/play.component';
 import {EditorComponent} from './pages/crosspic/editor/editor.component';
-import { ModalComponent } from './components/modal/modal.component';
+import {ModalComponent} from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
@@ -34,9 +43,13 @@ import { ModalComponent } from './components/modal/modal.component';
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, {metaReducers}),
+    StoreRouterConnectingModule,
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([]),
   ],
-  providers: [SystemService, {provide: LocationStrategy, useClass: HashLocationStrategy}],
+  providers: [SystemService, {provide: RouterStateSerializer, useClass: CustomRouterStateSerializer}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
